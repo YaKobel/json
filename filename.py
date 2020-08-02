4c6aac9aaf0c0dbbfa3d1606b2143740a128f8e3
Yaroslav Kobel <yarokobel@gmail.com>
2020-08-02 14:37:56 +0300

    refactoring code

--- filename.py
+++ filename.py
@@ -1,14 +1,28 @@
 import json
 
-filename = 'user.json'
+def get_username:
+    """Получает имя пользователя, если оно есть"""
+    filename = 'user.json'
+    try:
+        with open(filename) as f:
+            user = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return user
 
-try:
-    with open(filename) as f:
-        user = json.load(f)
-except:
-    username = input("Введите ваше имя: ")
-    with open(filename, 'w', encoding="utf-8") as fl:
-        json.dump(username, fl, ensure_ascii=False)
-        print("Мы запомним ваше имя как " + username + " !")
-else:
-    print("Добро пожаловать " + user + "!")
+
+def greet_user:
+    """Привтетсвие пользователя"""
+
+    username = get_username()
+    if username:
+        print("Добро пожаловать " + user + "!")
+    else:
+        username = input("Вдедите ваше имя: ")
+        filename = 'user.json'
+        with open(filename, 'w', encoding="utf-8") as fl:
+            json.dump(username, fl, ensure_ascii=False)
+            print("Мы запомним ваше имя, как " + username + " !")
+
+greet_user()
